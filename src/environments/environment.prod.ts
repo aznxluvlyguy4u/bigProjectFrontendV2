e0c06ba71aa9467461cb96147f4b6f69abc5e5f7
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-05-25 11:27:36 +0200

    Set environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  nsfoApiServerUrl: 'https://nsfo-api.jongensvantechniek.nl/api',
+  nsfoUserEnvUrl: 'https://online.nsfo.nl'
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  nsfoApiServerUrl: 'https://nsfo-dev-api.jongensvantechniek.nl/api',
+  nsfoUserEnvUrl: 'http://dev-online.nsfo.nl'
+};
