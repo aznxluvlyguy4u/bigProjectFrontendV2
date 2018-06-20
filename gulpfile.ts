a05d339b7e74670057ae7ad39550c927fcdb32c7
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-06-20 16:15:57 +0200

    Upload build files by gulp script

---
+++ gulpfile.ts
@@ -0,0 +1,81 @@
+const gulp = require('gulp');
+const env = require('gulp-env');
+
+const staging_user_frontend_bucket = 'dev-online.nsfo.nl';
+const production_user_frontend_bucket = 'online.nsfo.nl';
+const public_read = 'public-read';
+const retry_count = 5;
+const build_folder = 'dist/nsfo-frontend-new';
+
+/**
+ * Load environment variables from JSON File.
+ */
+
+env({
+    file: 'env.json'
+});
+
+
+/**
+ * Initialize AWS Config.
+ */
+
+const awsConfig = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    useIAM: true
+};
+
+const s3 = require('gulp-s3-upload')(awsConfig);
+
+/**
+ * Task to deploy build files directly to AWS S3 hosted website Bucket.
+ */
+
+gulp.task('deploy:staging', function() {
+  return gulp.src([build_folder + '/**/*'])
+    .pipe(s3({
+      Bucket: staging_user_frontend_bucket,
+      ACL:    public_read
+    }, {
+      maxRetries: retry_count
+    }))
+    ;
+});
+
+gulp.task('deploy:prod', function() {
+  return gulp.src([build_folder + '/**/*'])
+    .pipe(s3({
+      Bucket: production_user_frontend_bucket,
+      ACL:    public_read
+    }, {
+      maxRetries: retry_count
+    }))
+    ;
+});
+
+/**
+ * Task to deploy maintenance page directly to AWS S3 hosted website Bucket.
+ */
+
+gulp.task('deploy:staging:maintenance', function() {
+  return gulp.src(['src/maintenance-page/*'])
+    .pipe(s3({
+      Bucket: staging_user_frontend_bucket,
+      ACL:    public_read
+    }, {
+      maxRetries: retry_count
+    }))
+    ;
+});
+
+gulp.task('deploy:prod:maintenance', function() {
+  return gulp.src(['src/maintenance-page/*'])
+    .pipe(s3({
+      Bucket: production_user_frontend_bucket,
+      ACL:    public_read
+    }, {
+      maxRetries: retry_count
+    }))
+    ;
+});
