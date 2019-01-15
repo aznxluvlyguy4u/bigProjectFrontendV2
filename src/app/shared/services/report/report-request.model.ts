d29a8261be009560abf460ede6d96891ff4f5c16
Rudolf Sneep <rudolf.sneep@protonmail.com>
2019-01-15 12:16:43 +0100

    Add all report types for ReportType enum and report translations

--- src/app/shared/services/report/report-request.model.ts
+++ src/app/shared/services/report/report-request.model.ts
@@ -14,6 +14,13 @@ export class ReportRequest extends Worker {
   file_type: string;
 }
 
+/**
+ * Note that these enum values should match the ReportType enum class in the api
+ *
+ * When including extra report types,
+ * also update the logic in ReportModalComponent.getReportTypeString()
+ * and the translation files
+ */
 export enum ReportType {
   ANNUAL_ACTIVE_LIVE_STOCK = 1,
   ANNUAL_TE_100 = 2,
@@ -24,7 +31,25 @@ export enum ReportType {
   ANNUAL_ACTIVE_LIVE_STOCK_RAM_MATES = 7,
   OFF_SPRING = 8,
   PEDIGREE_REGISTER_OVERVIEW = 9,
-  LIVE_STOCK = 10
+  LIVE_STOCK = 10,
+  BIRTH_LIST = 11,
+  MEMBERS_AND_USERS_OVERVIEW = 12,
+  ANIMAL_HEALTH_STATUSES = 13,
+  QUICK_VIEW_KPIS = 14,
+  COMPANY_REGISTER = 15,
+  MARKS_PER_ANIMAL_PER_BIRTH_YEAR = 16,
+  CLIENT_NOTES_OVERVIEW = 17,
+  STAR_EWES = 18,
+  COMBI_FORMS_VKI_AND_TRANSPORT_DOCUMENTS = 19,
+  EWE_CARD = 20,
+  BLOOD_AND_TISSUE_INVESTIGATIONS = 21,
+  I_AND_R = 22,
+  POPREP_INPUT_FILE = 23,
+  REASONS_DEPART_AND_LOSS = 24,
+  WEIGHTS_PER_BIRTH_YEAR = 25,
+  TREATMENTS = 26,
+  MODEL_EXPORT_CERTIFICATE = 27,
+  ACTION_LOG = 28
 }
 
 export class ReportWorker {
