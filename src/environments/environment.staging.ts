7b151e41255d950be112ede3b0c6563bf4473409
Rudolf Sneep <rudolf.sneep@protonmail.com>
2019-01-16 16:54:33 +0100

    Set NSFO AWS urls in environment.staging.ts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  nsfoApiServerUrl: 'https://nsfo-dev-api.jongensvantechniek.nl/api',
-  nsfoUserEnvUrl: 'https://dev-online.nsfo.nl'
+  nsfoApiServerUrl: 'https://dev-api.nsfo.nl/api',
+  nsfoUserEnvUrl: 'https://dev-online2.nsfo.nl'
 };
