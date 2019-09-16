882ff40b026ced704a12fb793bdce66065577d7c
Rudolf Sneep <rudolf.sneep@protonmail.com>
2019-09-16 13:22:11 +0200

    Update REPORT_OPTIONS_LIST nl ordering

--- src/app/report/report.model.ts
+++ src/app/report/report.model.ts
@@ -1,8 +1,8 @@
 export const REPORT_OPTIONS_LIST = [
-  {url: '/main/report/lineage_proof', name: 'LINEAGE PROOFS', nl_order: 0},
-  {url: '/main/report/inbreeding', name: 'INBREEDING COEFFICIENT', nl_order: 1},
-  {url: '/main/report/offspring', name: 'OFFSPRING', nl_order: 2},
-  // {url: '/main/report/fertilizer_accounting', name: 'FERTILIZER ACCOUNTING', nl_order: 3},
-  {url: '/main/report/birth_list', name: 'BIRTH LIST', nl_order: 4},
-  {url: '/main/report/company_register', name: 'COMPANY REGISTER', nl_order: 5}
+  {url: '/main/report/lineage_proof', name: 'LINEAGE PROOFS', nl_order: 0}, // Afstammingsbewijzen
+  {url: '/main/report/company_register', name: 'COMPANY REGISTER', nl_order: 1}, // Bedrijfsregister,
+  {url: '/main/report/birth_list', name: 'BIRTH LIST', nl_order: 2}, // Geboortelijst
+  {url: '/main/report/inbreeding', name: 'INBREEDING COEFFICIENT', nl_order: 3}, // Inteeltcoëfficient
+  // {url: '/main/report/fertilizer_accounting', name: 'FERTILIZER ACCOUNTING', nl_order: 4}, // Mestboekhouding
+  {url: '/main/report/offspring', name: 'OFFSPRING', nl_order: 5} // Nakomelingen
 ];
