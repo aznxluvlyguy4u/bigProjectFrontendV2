192e95663b54241df4388aa5f85e3080c4b8fdb6
davidmesman <david@uptimemonkeys.com>
2020-06-19 16:07:35 +0200

    Changes methods, variables, and others to reflect the changes made to medications

--- src/app/shared/models/treatment-medication.model.ts
+++ src/app/shared/models/treatment-medication.model.ts
@@ -2,5 +2,11 @@
 export class TreatmentMedication {
   id: number;
   name: string;
+  dosage: number;
+  dosage_unit: string;
+  reg_nl: string;
+  treatment_duration: number;
+  waiting_days: number;
   is_active: boolean;
+  waiting_time_end;
 }
