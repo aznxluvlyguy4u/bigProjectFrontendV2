2d93c749b845a1680b56a5bde38047a113a16896
Yubin Chen <yubin@jongensvantechniek.nl>
2020-08-17 12:57:41 +0200

    Activate invoices tab

--- src/app/shared/variables/feature.activation.ts
+++ src/app/shared/variables/feature.activation.ts
@@ -1,4 +1,4 @@
-export const IS_INVOICES_ACTIVE = false;
+export const IS_INVOICES_ACTIVE = true;
 export const IS_REGISTRATIONS_ACTIVE = true;
 export const IS_TREATMENTS_ACTIVE = true;
 export const IS_HEALTH_MODULE_ACTIVE = false;
